34f5b39740e0fb7240ee959cb2f34eebcf57956b
Jawahar96 <97747021+Jawahar96@users.noreply.github.com>
2022-10-19 18:35:27 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,13 @@
+const resume = 
+    [{
+      "MailId":"jawaharsabesan@gmail.com",
+    "name": "Jawahar",
+    "work":"Fresher"
+}]
+    [{
+    "qualification":"MCA",
+    "place":"Thanjavur"
+}]
+
+console.log(resume);
+console.log(a[0].MailId);
